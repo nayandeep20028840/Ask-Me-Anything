a0c4755176b919449845e06eba081cbb9722f302
Nayan Deep <97220336+nayandeep20028840@users.noreply.github.com>
2025-02-15 12:36:33 +0530

    build error

--- src/app/api/delete-message/[messageid]/route.ts
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -1,32 +1,42 @@
-import UserModel from '@/model/User';
-import { getServerSession } from 'next-auth/next';
-import dbConnect from '@/lib/dbConnect';
-import { User } from 'next-auth';
-import { authOptions } from '../../auth/[...nextauth]/options';
+import dbConnect from "@/lib/dbConnect";
+import { User, getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/options";
+import UserModel from "@/model/User";
+
 
 export async function DELETE(
     request: Request,
-    { params }: { params: { messageid: string } }
+    { params }: { params: { messageId: string } }
 ) {
-    const messageId = params.messageid;
+    // console.log("hello",params);
+    // console.log("world",params.messageId);
 
+    // return Response.json({
+    //     message : "nayan"
+    // })
+
+
+    const messageId = params.messageId as string;
     await dbConnect();
 
     const session = await getServerSession(authOptions);
 
-    const _user: User = session?.user as User;
-    if (!session || !_user) {
-        return Response.json(
-            { success: false, message: 'Not authenticated' },
-            { status: 401 }
-        );
+    const user: User = session?.user as User;
+
+    if (!session || !user) {
+        return Response.json({
+            success: false,
+            message: "User not authenticated"
+        }, {
+            status: 401
+        });
     }
 
     try {
         const updateResult = await UserModel.updateOne(
-            { _id: _user._id },
+            { _id: user._id },
             { $pull: { messages: { _id: messageId } } }
-        );
+        )
 
         if (updateResult.modifiedCount === 0) {
             return Response.json(
@@ -35,15 +45,15 @@ export async function DELETE(
             );
         }
 
-        return Response.json(
-            { message: 'Message deleted', success: true },
-            { status: 200 }
-        );
+        return Response.json({
+            success: true,
+            message: "Message deleted successfully"
+        });
     } catch (error) {
         console.error('Error deleting message:', error);
         return Response.json(
             { message: 'Error deleting message', success: false },
             { status: 500 }
         );
-    }
+    } 
 }
